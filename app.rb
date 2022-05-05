7a98820e0601394894c65d9b557d2a8c40ba8370
nonnax <ironald@gmail.com>
2022-05-05 23:30:15 +0800

    add sample

--- app.rb
+++ app.rb
@@ -3,21 +3,24 @@
 require 'rack'
 require_relative 'lib/basic'
 
-DOC=<<~___
-VERA FILES FACT CHECK: Columnist Tundag falsely claims Marcos Jr. ‘accepted’ dismissal of 2016 election protest
-By VERA FILES
-Apr 27, 2022 6:18 PM
-
-Columnist Jerry Suico Tundag of Cebu-based newspaper The Freeman falsely claimed in a tweet that presidential candidate Ferdinand “Bongbong” Marcos Jr.  had “accepted” the verdict of the Presidential Electoral Tribunal (PET), which dismissed in its entirety his poll protest against Vice President Leni Robredo six years ago.
-___
+def load
+  db=[]
+  Dir.chdir 'public/models' do
+      Dir['*'].each do |f|
+         db << eval(IO.popen(["yaml", f], &:read))
+      end
+  end
+  db
+end
 
 Basic.routes do
-
-  set '/',  (1..20).inject(''){|acc, s|
-              acc<< %(<div class="item">
-                  <h1><a href='/show?item=#{s}'><%=@data[:title]%></a></h1>
-                  <p><%=@data[:doc][0..120]%></p></div>)
-                  }
+  @database = load
+  set '/',  @database.map.with_index{|h, i|
+              h.transform_keys(&:to_sym)=>{tag:, body:}
+              %( <div class="item">
+                <h1><a href='/show?item=#{i}'>#{tag}</a></h1>
+                <p>"#{body[0]}"</p></div> )
+            }.join
 
   set '/home',  :index
 
@@ -27,7 +30,9 @@ end
 
 class App < Basic::App
   def get
+    db = load
     name, @status = fetch(env, default: :index).values_at(:name, :status)
-    erb name, title:'File item', doc: DOC, params: req.params
+
+    erb name, title:'File item', doc: db[req.params['item'].to_i], params: req.params
   end
 end
